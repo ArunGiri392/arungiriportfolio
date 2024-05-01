1452fb86b40e98227caa851cc6d64d9cb6fb895b
Arun Giri <AGiri09@my.fisk.edu>
2024-05-01 13:40:45 -0500

    changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,7 @@ import { Nav, Logo, NavLink, Bars, NavMenu, NavBtn } from "./HeaderElements";
 import "./style.css";
 
 const Header = ({ toggle }) => {
-  // const [isCertificationsOpen, setIsCertificationsOpen] = useState(false);
-
-  // const handleCertificationsClick = () => {
-  //   setIsCertificationsOpen(!isCertificationsOpen);
-  // };
+  
 
   return (
     <div className="Container" style={{ padding: 0 }}>
@@ -20,37 +16,6 @@ const Header = ({ toggle }) => {
             Experiences
           </NavLink>
 
-          {/* Certifications Dropdown
-          <div className="menu-item">
-            <NavLink
-              className="menu-item"
-              onClick={handleCertificationsClick}
-              style={{ color: "rgb(119, 119, 121)", cursor: "pointer" }}>
-              Certifications
-            </NavLink>
-            {isCertificationsOpen && (
-              <div className="dropdown-content">
-                <a
-                  href="#/action-1"
-                  onClick={() => {
-                    window.open(
-                      "https://www.credly.com/badges/45d979c2-afd3-4570-b837-ddb1371c746c/linked_in?t=s32xk4"
-                    );
-                  }}>
-                  AWS SAA-C03
-                </a>
-                <a
-                  href="#/action-2"
-                  onClick={() => {
-                    window.open(
-                      "https://www.coursera.org/account/accomplishments/certificate/TCHXK5NKMS8Z"
-                    );
-                  }}>
-                  ML/AI(Coursera)
-                </a>
-              </div>
-            )}
-          </div> */}
 
           <NavLink className="menu-item" to="about">
             About
