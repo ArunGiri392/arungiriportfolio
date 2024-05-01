45187b2a08fc56c23876cac9a90c330a1b49802b
Arun Giri <AGiri09@my.fisk.edu>
2024-05-01 14:20:28 -0500

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css'; // Assuming you have some global styles defined
+import App from './App'; // Adjust the path if your App component is in a different folder
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Get the element where your React app will attach
+const container = document.getElementById('root');
+
+// Create a root for the app using the container
+const root = createRoot(container);
+
+// Render your App component to the DOM using the root
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you have a service worker and it's part of your project setup:
+// import reportWebVitals from './reportWebVitals';
+// import * as serviceWorker from './serviceWorker';
+// serviceWorker.unregister();
+// reportWebVitals();
